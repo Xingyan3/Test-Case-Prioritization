71327d233d2246b6bf37e9c114ed5b5556ccc425
Zhaorui Yang <zyang247@ucr.edu>
2024-02-20 00:17:04 +0000

    Add baseline coverage statistics. Total statement coverage finished, to be verified.

--- src/run_benchmarks.py
+++ src/run_benchmarks.py
@@ -3,49 +3,23 @@ import sys
 import argparse
 import json
 
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-c', '--criteria', 
-                        choices=['statement', 'branch'], 
-                        default='statement',
-                        help='Choose coverage criteria from "statement" or "branch".')
-arg_parser.add_argument('-p', '--prioritization',
-                        choices=['baseline', 'random', 'total_cov', 'additional_cov'],
-                        default='baseline',
-                        help='Choose from "baseline", "random", "total_cov", "additional_cov". Default is "baseline".')
+def parse_benchmark_list(args):
+  
+  valid_choices = ['tcas', 'totinfo', 'schedule', 'schedule2', 'printtokens', 'printtokens2', 'replace']
 
-arg_parser.add_argument('-b', '--benchmark',
-                        nargs='+',
-                        default='schedule2',
-                        help='Choose a benchmark to run. Default is "schedule2". Can pass "all".')
+  if 'all' in args.benchmark:
+    benchmarks = valid_choices
+  else:
+    benchmarks = args.benchmark
 
-args = arg_parser.parse_args()
+  return benchmarks
 
-os.chdir(sys.path[0])
+def goto_benchmark_dir_and_clean(bm):
+  project = os.path.join('.', bm)
+  os.chdir(project)
 
-bm_relative_path = "../benchmarks"
-
-os.chdir(bm_relative_path)
-# benchmarks = os.listdir(".")
-
-# # for bm in benchmarks:
-
-# #   project = os.path.join(".", bm)
-# #   os.chdir(project)
-# #   os.system("make clean")
-# #   os.system("make")
-
-# #   with open("universe.txt", 'r') as f:
-# #     for execution_args in f:
-# #       os.system(project + " " + execution_args)
-
-
-# #   os.chdir("..")
-
-
-# exmaple
-# schedule2
-# statement
-# total coverage
+  os.system("make clean")
+  os.system("make")
 
 class StatementCoverage:
   def __init__(self, testid=None, total_line_no=None, executed_line_no=None) -> None:
@@ -59,29 +33,29 @@ class StatementCoverage:
 
   def to_dict(self):
     return {
-            # "total_line_no": list(self.total_line_no),
-            # "executed_line_no": list(self.executed_line_no),
-            "testid": self.testid,
-            "statement_cov_rate": self.statement_coverage()}
+            "testid": self.testid,  
+            "total_line_num": len(self.total_line_no),
+            "executed_line_num": len(self.executed_line_no),
+            "statement_coverage": self.statement_coverage()}
 
   def __str__(self) -> str:
-    return str(self.testid) + str(self.total_line_no) + '%' + str(self.executed_line_no) + '%' + str(self.statement_coverage())
+    return 'test id: ' + str(self.testid) + '\ntotal line num: ' + str(len(self.total_line_no)) + '\nexecuted line num: ' + str(len(self.executed_line_no)) + '\nstatement coverage: ' + str(self.statement_coverage())
 
 
+def total_statement(bm):
 
-valid_choices = ['tcas', 'totinfo', 'schedule2', 'schedule', 'print_tokens', 'replace', 'print_tokens2']
-
-if 'all' in args.benchmark:
-  benchmarks = valid_choices
-else:
-  benchmarks = args.benchmark
-
-for bm in benchmarks:
   bm_src = bm + '.c'
   project = os.path.join('.', bm)
-  os.chdir(project)
-  os.system("make clean")
-  os.system("make")
+
+  goto_benchmark_dir_and_clean(bm)
+  os.system('rm -rf my_test_suite.txt')
+  
+  with open('baseline.json', 'r') as f:
+    baseline = json.load(f)
+
+  baseline_executed_line_num = baseline['executed_line_num']
+  total_line_num = baseline['total_line_num']
+
   test_coverages = []
   test_count = 0
   with open("universe.txt", 'r') as f:
@@ -95,47 +69,131 @@ for bm in benchmarks:
       with open(cov_data_file) as cov_f:
         cov_data = json.load(cov_f)
 
-      if len(cov_data["files"]) == 1:
-        file = cov_data["files"][0]
-        sc = StatementCoverage()
-        sc.total_line_no = {x['line_number'] for x in file['lines']}
-        sc.executed_line_no = {x['line_number'] for x in file['lines'] if x['count']>=1}
-        sc.testid = tid
-      else:
+      if len(cov_data["files"]) != 1:
         sys.exit('Too many files(>1) to coverage')
 
+      
+      file = cov_data["files"][0]
+      sc = StatementCoverage()
+      sc.total_line_no = {x['line_number'] for x in file['lines']}
+      sc.executed_line_no = {x['line_number'] for x in file['lines'] if x['count']>=1}
+      sc.testid = tid
+        
       test_coverages.append(sc)
       # delete gcda, json files
       os.remove(bm + '.gcda')
       os.remove(cov_data_file)
 
-  dp = test_coverages[0].total_line_no
+  # check if all test cases have the same total line numbers
   for j in range(1, len(test_coverages)):
-    if dp != test_coverages[j].total_line_no:
-      sys.exit(f'Program total line numbers difference. dp:{len(dp)}, test_coverages[{j}]:{len(test_coverages[j].total_line_no)}')
-  print(f'All test cases have the same total line numbers: {len(dp)}')
+    if total_line_num != len(test_coverages[j].total_line_no):
+      sys.exit(f'Program total line numbers difference. total_line_num:{total_line_num}, test_coverages[{j}]:{len(test_coverages[j].total_line_no)}')
+  print(f'All test cases have the same total line numbers: {total_line_num}')
 
   # sort by statement coverage
-  test_coverages.sort(key=lambda x: x.statement_coverage())
+  test_coverages.sort(key=lambda x: len(x.executed_line_no), reverse=True)
   test_suite = [test_coverages[0].testid]
   coverd_lines = test_coverages[0].executed_line_no
   for idx in range(1, len(test_coverages)):
-
-    if len(coverd_lines) == len(dp):
+    # with open('log.txt', 'a') as f:
+    #   f.write(f'coverd_lines: {len(coverd_lines)}, baseline_executed_line_num: {baseline_executed_line_num}\n')
+    if len(coverd_lines) >= baseline_executed_line_num:
+      # print(f'coverd_lines: {len(coverd_lines)}, baseline_executed_line_num: {baseline_executed_line_num}')
       break
-
-    if coverd_lines < test_coverages[idx].executed_line_no:
+    
+    diff = test_coverages[idx].executed_line_no.difference(coverd_lines)
+    if len(diff) > 0:
       test_suite.append(test_coverages[idx].testid)
-      coverd_lines = coverd_lines.union(test_coverages[idx].executed_line_no)
+      coverd_lines = coverd_lines.union(diff)
 
   with open("universe.txt", 'r') as f:
     lines = f.readlines()
     new_tests = [lines[i] for i in test_suite]
     with open("my_test_suite.txt", 'w') as f:
       f.writelines(new_tests)
+
+  print(f'test_suite len: {len(test_suite)}, test_count: {test_count}')
+  print (f'test_suite: {test_suite}')
+  print(f'coverd_lines: {len(coverd_lines)}, baseline_executed_line_num: {baseline_executed_line_num}')
+  os.chdir('..')
     
 
+def baseline_statement_coverage(bm):
+  
+  bm_src = bm + '.c'
+  project = os.path.join('.', bm)
+
+  goto_benchmark_dir_and_clean(bm)
+  os.remove('baseline.json')
+
+  test_count = 0
+  with open("universe.txt", 'r') as f:
+    for tid, execution_args in enumerate(f):
+      test_count += 1
+      os.system(project + " " + execution_args)
+  
+  os.system("gcov " + "--json-format " + bm_src)
+
+  cov_data_file = bm_src + ".gcov" + ".json"
+  os.system("gunzip " + cov_data_file + '.gz')
+
+  # parse json
+  with open(cov_data_file) as cov_f:
+    cov_data = json.load(cov_f)
+
+  if len(cov_data["files"]) != 1:
+    sys.exit('Too many files(>1) to coverage')
+
+  file = cov_data["files"][0]
+  sc = StatementCoverage()
+  sc.total_line_no = {x['line_number'] for x in file['lines']}
+  sc.executed_line_no = {x['line_number'] for x in file['lines'] if x['count']>=1}
+  
+  print(f'Baseline statement coverage for {bm}: {sc.statement_coverage()}')
+  with open("baseline.json", 'w') as f:
+    d = sc.to_dict()
+    d['test_count'] = test_count
+    json.dump(d, f, indent=2)
+    # f.write('\n' + str(test_count) + ' test cases\n')
+
+  os.remove(bm + '.gcda')
+  os.remove(cov_data_file)
+  os.chdir('..')
+
+def main():
+  arg_parser = argparse.ArgumentParser()
+
+  arg_parser.add_argument('-c', '--criteria', 
+                          choices=['statement', 'branch'], 
+                          default='statement',
+                          help='Choose coverage criteria from "statement" or "branch".')
+
+  arg_parser.add_argument('-p', '--prioritization',
+                          choices=['baseline', 'random', 'total_cov', 'additional_cov'],
+                          default='baseline',
+                          help='Choose from "baseline", "random", "total_cov", "additional_cov". Default is "baseline".')
+
+  arg_parser.add_argument('-b', '--benchmark',
+                          nargs='+',
+                          default='schedule2',
+                          help='Choose a benchmark to run. Default is "schedule2". Can pass "all".')
+
+  args = arg_parser.parse_args()
+
+  os.chdir(sys.path[0])
+
+  os.chdir('../benchmarks')
+  benchmarks = parse_benchmark_list(args)
+
+  if args.prioritization == 'baseline' and args.criteria == 'statement':
+    for bm in benchmarks:
+      baseline_statement_coverage(bm)
+
+  if args.prioritization == 'total_cov' and args.criteria == 'statement':
+    for bm in benchmarks:
+      total_statement(bm)
+
+if __name__ == '__main__':
+  main()
 
 
-# print(f'test_suite len: {len(test_suite)}, test_count: {test_count}')
-# print (f'test_suite: {test_suite}')

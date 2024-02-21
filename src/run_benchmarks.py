fc343e8b3b75b5d1603b6a7fdbbffb7234fcdef3
Zhaorui Yang <zyang247@ucr.edu>
2024-02-21 09:54:30 +0000

    Add verify statement-total function to verify statement-cov-total-prior-filtered test suite correctness

--- src/run_benchmarks.py
+++ src/run_benchmarks.py
@@ -3,24 +3,6 @@ import sys
 import argparse
 import json
 
-def parse_benchmark_list(args):
-  
-  valid_choices = ['tcas', 'totinfo', 'schedule', 'schedule2', 'printtokens', 'printtokens2', 'replace']
-
-  if 'all' in args.benchmark:
-    benchmarks = valid_choices
-  else:
-    benchmarks = args.benchmark
-
-  return benchmarks
-
-def goto_benchmark_dir_and_clean(bm):
-  project = os.path.join('.', bm)
-  os.chdir(project)
-
-  os.system("make clean")
-  os.system("make")
-
 class StatementCoverage:
   def __init__(self, testid=None, total_line_no=None, executed_line_no=None) -> None:
     self.testid = testid
@@ -42,13 +24,33 @@ class StatementCoverage:
     return 'test id: ' + str(self.testid) + '\ntotal line num: ' + str(len(self.total_line_no)) + '\nexecuted line num: ' + str(len(self.executed_line_no)) + '\nstatement coverage: ' + str(self.statement_coverage())
 
 
+def parse_benchmark_list(args):
+  
+  valid_choices = ['tcas', 'totinfo', 'schedule', 'schedule2', 'printtokens', 'printtokens2', 'replace']
+
+  if 'all' in args.benchmark:
+    benchmarks = valid_choices
+  else:
+    benchmarks = args.benchmark
+
+  return benchmarks
+
+
+def goto_benchmark_dir_and_clean(bm):
+  project = os.path.join('.', bm)
+  os.chdir(project)
+
+  os.system("make clean")
+  os.system("make")
+
+
 def total_statement(bm):
 
   bm_src = bm + '.c'
   project = os.path.join('.', bm)
 
   goto_benchmark_dir_and_clean(bm)
-  os.system('rm -rf my_test_suite.txt')
+  os.system('rm -rf test_suite_statement_total.txt')
   
   with open('baseline.json', 'r') as f:
     baseline = json.load(f)
@@ -109,7 +111,7 @@ def total_statement(bm):
   with open("universe.txt", 'r') as f:
     lines = f.readlines()
     new_tests = [lines[i] for i in test_suite]
-    with open("my_test_suite.txt", 'w') as f:
+    with open("test_suite_statement_total.txt", 'w') as f:
       f.writelines(new_tests)
 
   print(f'test_suite len: {len(test_suite)}, test_count: {test_count}')
@@ -118,36 +120,39 @@ def total_statement(bm):
   os.chdir('..')
     
 
+def accumulate_execution(executable_name, test_path):
+  test_count = 0
+  with open(test_path, 'r') as f:
+    for line in f:
+      test_count += 1
+      os.system(executable_name + " " + line)
+    
+  os.system("gcov " + "--json-format " + executable_name + ".c")
+  cov_data_file = executable_name + ".c" + ".gcov" + ".json"
+  os.system("gunzip " + cov_data_file + '.gz')
+  with open(cov_data_file) as cov_f:
+    cov_data = json.load(cov_f)
+  if len(cov_data["files"]) != 1:
+    sys.exit('Too many files(>1) to coverage')
+  cov_file = cov_data["files"][0]
+  sc = StatementCoverage() 
+  sc.total_line_no = {x['line_number'] for x in cov_file['lines']}
+  sc.executed_line_no = {x['line_number'] for x in cov_file['lines'] if x['count']>=1}
+
+  os.remove(executable_name + '.gcda')
+  os.remove(cov_data_file)
+
+  return sc, test_count
+
+
 def baseline_statement_coverage(bm):
   
-  bm_src = bm + '.c'
   project = os.path.join('.', bm)
 
   goto_benchmark_dir_and_clean(bm)
   os.remove('baseline.json')
 
-  test_count = 0
-  with open("universe.txt", 'r') as f:
-    for tid, execution_args in enumerate(f):
-      test_count += 1
-      os.system(project + " " + execution_args)
-  
-  os.system("gcov " + "--json-format " + bm_src)
-
-  cov_data_file = bm_src + ".gcov" + ".json"
-  os.system("gunzip " + cov_data_file + '.gz')
-
-  # parse json
-  with open(cov_data_file) as cov_f:
-    cov_data = json.load(cov_f)
-
-  if len(cov_data["files"]) != 1:
-    sys.exit('Too many files(>1) to coverage')
-
-  file = cov_data["files"][0]
-  sc = StatementCoverage()
-  sc.total_line_no = {x['line_number'] for x in file['lines']}
-  sc.executed_line_no = {x['line_number'] for x in file['lines'] if x['count']>=1}
+  sc, test_count = accumulate_execution(project, 'universe.txt')
   
   print(f'Baseline statement coverage for {bm}: {sc.statement_coverage()}')
   with open("baseline.json", 'w') as f:
@@ -156,9 +161,32 @@ def baseline_statement_coverage(bm):
     json.dump(d, f, indent=2)
     # f.write('\n' + str(test_count) + ' test cases\n')
 
-  os.remove(bm + '.gcda')
-  os.remove(cov_data_file)
   os.chdir('..')
+
+
+def verify_statement_total(bm):
+  bm_src = bm + '.c'
+  project = os.path.join('.', bm)
+
+  goto_benchmark_dir_and_clean(bm)
+
+  sc, _ = accumulate_execution(project, 'test_suite_statement_total.txt')
+
+  with open('baseline.json', 'r') as f:
+    baseline = json.load(f)
+  
+  baseline_cov = baseline['statement_coverage']
+
+  if round(sc.statement_coverage(), 2) != round(baseline_cov, 2):
+    sys.exit(f'Failed to cover all lines. statement_coverage: {sc.statement_coverage()}, baseline_cov: {baseline_cov}')
+  
+  print(f'Verified statement coverage for {bm}: {sc.statement_coverage()}')
+
+  os.chdir('..')
+
+
+def random_statement(bm):
+  pass
 
 def main():
   arg_parser = argparse.ArgumentParser()
@@ -178,6 +206,10 @@ def main():
                           default='schedule2',
                           help='Choose a benchmark to run. Default is "schedule2". Can pass "all".')
 
+  arg_parser.add_argument('-v', '--verify',
+                          action='store_true',
+                          help='Verify the test suite.')
+
   args = arg_parser.parse_args()
 
   os.chdir(sys.path[0])
@@ -185,13 +217,18 @@ def main():
   os.chdir('../benchmarks')
   benchmarks = parse_benchmark_list(args)
 
-  if args.prioritization == 'baseline' and args.criteria == 'statement':
+  if (not args.verify) and args.prioritization == 'baseline' and args.criteria == 'statement':
     for bm in benchmarks:
       baseline_statement_coverage(bm)
 
-  if args.prioritization == 'total_cov' and args.criteria == 'statement':
+  if (not args.verify) and args.prioritization == 'total_cov' and args.criteria == 'statement':
     for bm in benchmarks:
       total_statement(bm)
+
+  if args.verify and args.criteria == 'statement' and args.prioritization == 'total_cov':
+    for bm in benchmarks:
+      verify_statement_total(bm)
+    
 
 if __name__ == '__main__':
   main()
